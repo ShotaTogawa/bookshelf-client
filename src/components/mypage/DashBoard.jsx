a82e01503627e105dc53570da70fa64b17b5305f
ShotaTogawa <s.togawa0112@gmail.com>
2019-11-29 11:41:19 -0800

    wrote content html

--- src/components/mypage/DashBoard.jsx
+++ src/components/mypage/DashBoard.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import Header from "./header/Header";
-import SideMenu from "./SideMenu/SideMenu";
+import SideMenu from "./sidemenu/SideMenu";
+import Content from "./content/Content";
 
 class DashBoard extends Component {
   render() {
     return (
       <div>
         <Header />
-        <SideMenu />
+        <main className="main-content">
+          <SideMenu />
+          <Content />
+        </main>
       </div>
     );
   }
---
+++ src/components/mypage/content/Content.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+  render() {
+    return (
+      <div class="content">
+        <h2 class="heading">Main</h2>
+      </div>
+    );
+  }
+}
+
+export default Content;

a6e29c8dfc437f9db637cebaa2e096d45bc58657
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-04 22:50:12 -0800

    make each table comonent be independent and add todo

--- src/components/mypage/content/table/BookTable.jsx
+++ src/components/mypage/content/table/BookTable.jsx
@@ -1,9 +1,10 @@
 import React, { Component, Fragment } from "react";
 import SideMenu from "../../sidemenu/SideMenu";
+import BeforeReading from "./BeforeReading";
+import Reading from "./Reading";
+import Read from "./Read";
 import { connect } from "react-redux";
 import { fetchBooks } from "../../../../actions";
-import Spinner from "../../../../spinner/Spinner";
-import moment from "moment";
 import {
   tableHeaderBefore,
   tableHeaderReading,
@@ -56,42 +57,6 @@ class BookTable extends Component {
         return <Table.HeaderCell>{header}</Table.HeaderCell>;
       });
     }
-
-    return tableHeaderBefore.map(header => {
-      return <Table.HeaderCell>{header}</Table.HeaderCell>;
-    });
-  };
-
-  renderTableData = () => {
-    // const local = JSON.parse(localStorage.getItem("user"));
-    if (!this.props.books) {
-      return <Spinner />;
-    }
-    return this.props.books.map(data => {
-      console.log(data);
-      return (
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-              <Image
-                src="https://image.shutterstock.com/image-photo/beautiful-water-drop-on-dandelion-260nw-789676552.jpg"
-                size="tiny"
-              />
-            </Table.Cell>
-            <Table.Cell>{data.name}</Table.Cell>
-            <Table.Cell>{data.genre}</Table.Cell>
-            <Table.Cell>{data.author}</Table.Cell>
-            <Table.Cell>{data.page_nums}</Table.Cell>
-            <Table.Cell>
-              {moment(data.createdAt).format("MMM D YYYY")}
-            </Table.Cell>
-            <Table.Cell>
-              {moment(data.startDate).format("MMM D YYYY")}
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      );
-    });
   };
 
   render() {
@@ -121,7 +86,13 @@ class BookTable extends Component {
             <Table.Header>
               <Table.Row>{this.renderTableHeader()}</Table.Row>
             </Table.Header>
-            {this.renderTableData()}
+            {this.state.activeItem === "Reading" ? (
+              <Reading books={this.props.books} />
+            ) : this.state.activeItem === "Read" ? (
+              <Read books={this.props.books} />
+            ) : (
+              <BeforeReading books={this.props.books} />
+            )}
           </Table>
         </Grid.Column>
       </Grid>
